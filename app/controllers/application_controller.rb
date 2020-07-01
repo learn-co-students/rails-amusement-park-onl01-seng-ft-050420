0ba4b04e42c9e627f6aff2ecd9974a72dae96e38
github username <lushiyun@outlook.com>
2020-07-01 00:54:40 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    !!current_user
+  end
+  
+  def current_user
+    @user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+
+  resources :users, only: [:new, :create, :show]
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  
 end
