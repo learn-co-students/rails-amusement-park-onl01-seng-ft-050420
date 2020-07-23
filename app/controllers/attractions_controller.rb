794e616dc4c87e8dd2534269e94d9bb493c55585
mkopsho <michael.kopsho@gmail.com>
2020-07-23 16:08:37 -0400

    Done

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -26,4 +26,10 @@ class AttractionsController < ApplicationController
     attraction.update(attraction_params)
     redirect_to attraction_path(attraction)
   end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+  end
 end
